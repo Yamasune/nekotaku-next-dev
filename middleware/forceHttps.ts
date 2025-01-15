3a4137322de87292157a5dfae10394f631d910cd
KUN1007 <yuyuyukunkunkun@gmail.com>
2025-01-15 16:55:01 +0800

    refactor: forceHttps middleware

--- middleware/forceHttps.ts
+++ middleware/forceHttps.ts
@@ -2,10 +2,14 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export const kunForceHttpsMiddleware = async (request: NextRequest) => {
-  if (request.nextUrl.protocol === 'http:') {
+  const protocol = request.headers.get('X-Forwarded-Proto') || 'https'
+
+  if (protocol === 'http') {
     const httpsUrl = new URL(request.url)
     httpsUrl.protocol = 'https:'
+    httpsUrl.hostname = process.env.NEXT_PUBLIC_KUN_PATCH_ADDRESS_PROD!
     return NextResponse.redirect(httpsUrl)
   }
+
   return NextResponse.next()
 }
