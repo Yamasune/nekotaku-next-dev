c557effe21367127697eb866216eac51b6bb8b8a
KUN1007 <yuyuyukunkunkun@gmail.com>
2025-01-25 00:22:28 +0800

    fix: getNSFWHeader

--- utils/actions/getNSFWHeader.ts
+++ utils/actions/getNSFWHeader.ts
@@ -4,6 +4,17 @@ import { cookies } from 'next/headers'
 
 export const getNSFWHeader = async () => {
   const cookieStore = await cookies()
-  const nsfwCookie = cookieStore.get('nsfw')
-  return nsfwCookie?.value === 'true' ? {} : { nsfw: undefined }
+  const token = cookieStore.get(
+    'kun-patch-setting-store|state|data|kunNsfwEnable'
+  )?.value
+
+  if (!token) {
+    return { content_limit: 'sfw' }
+  }
+
+  if (token === 'all') {
+    return {}
+  } else {
+    return { content_limit: token }
+  }
 }
