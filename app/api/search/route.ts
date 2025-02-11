1997f4904392264277eeb085192e45f7413d9054
KUN1007 <yuyuyukunkunkun@gmail.com>
2025-02-11 22:26:56 +0800

    feat: search page partial matching for aliases

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -24,15 +24,27 @@ export const searchGalgame = async (
           ...(searchOption.searchInIntroduction
             ? [{ introduction: { contains: q, mode: insensitive } }]
             : []),
-          ...(searchOption.searchInAlias ? [{ alias: { has: q } }] : []),
-          ...(searchOption.searchInTag
-            ? [{
-                tag: {
-                  some: {
-                    tag: { name: { contains: q, mode: insensitive } }
+          ...(searchOption.searchInAlias
+            ? [
+                {
+                  alias: {
+                    some: {
+                      name: { contains: q, mode: insensitive }
+                    }
                   }
                 }
-              }]
+              ]
+            : []),
+          ...(searchOption.searchInTag
+            ? [
+                {
+                  tag: {
+                    some: {
+                      tag: { name: { contains: q, mode: insensitive } }
+                    }
+                  }
+                }
+              ]
             : [])
         ]
       })),
@@ -53,15 +65,27 @@ export const searchGalgame = async (
           ...(searchOption.searchInIntroduction
             ? [{ introduction: { contains: q, mode: insensitive } }]
             : []),
-          ...(searchOption.searchInAlias ? [{ alias: { hasSome: [q] } }] : []),
-          ...(searchOption.searchInTag
-            ? [{
-                tag: {
-                  some: {
-                    tag: { name: { contains: q, mode: insensitive } }
+          ...(searchOption.searchInAlias
+            ? [
+                {
+                  alias: {
+                    some: {
+                      name: { contains: q, mode: insensitive }
+                    }
                   }
                 }
-              }]
+              ]
+            : []),
+          ...(searchOption.searchInTag
+            ? [
+                {
+                  tag: {
+                    some: {
+                      tag: { name: { contains: q, mode: insensitive } }
+                    }
+                  }
+                }
+              ]
             : [])
         ]
       })),
@@ -77,7 +101,6 @@ export const searchGalgame = async (
 
   return { galgames, total }
 }
-
 
 export const POST = async (req: NextRequest) => {
   const input = await kunParsePostBody(req, searchSchema)
