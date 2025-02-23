f1415a86f23debe421f004b474a362a40852a145
KUN1007 <yuyuyukunkunkun@gmail.com>
2025-02-23 23:44:55 +0800

    feat: formatNumber for CardStats

--- components/kun/CardStats.tsx
+++ components/kun/CardStats.tsx
@@ -1,6 +1,7 @@
 import { Tooltip } from '@nextui-org/tooltip'
 import { Download, Eye, Heart, MessageSquare, Puzzle } from 'lucide-react'
 import { cn } from '~/utils/cn'
+import { formatNumber } from '~/utils/formatNumber'
 
 interface Props {
   patch: GalgameCard
@@ -24,21 +25,21 @@ export const KunCardStats = ({
       <Tooltip isDisabled={disableTooltip} content="浏览数" placement="bottom">
         <div className="flex items-center gap-1">
           <Eye className="size-4" />
-          <span>{patch.view}</span>
+          <span>{formatNumber(patch.view)}</span>
         </div>
       </Tooltip>
 
       <Tooltip isDisabled={disableTooltip} content="下载数" placement="bottom">
         <div className="flex items-center gap-1">
           <Download className="size-4" />
-          <span>{patch.download}</span>
+          <span>{formatNumber(patch.download)}</span>
         </div>
       </Tooltip>
 
       <Tooltip isDisabled={disableTooltip} content="收藏数" placement="bottom">
         <div className="flex items-center gap-1">
           <Heart className="size-4" />
-          <span>{patch._count.favorite_by || 0}</span>
+          <span>{formatNumber(patch._count.favorite_by || 0)}</span>
         </div>
       </Tooltip>
 
@@ -50,7 +51,7 @@ export const KunCardStats = ({
         >
           <div className="flex items-center gap-1">
             <Puzzle className="size-4" />
-            <span>{patch._count.resource || 0}</span>
+            <span>{formatNumber(patch._count.resource || 0)}</span>
           </div>
         </Tooltip>
       )}
@@ -58,7 +59,7 @@ export const KunCardStats = ({
       <Tooltip isDisabled={disableTooltip} content="评论数" placement="bottom">
         <div className="flex items-center gap-1">
           <MessageSquare className="size-4" />
-          <span>{patch._count.comment || 0}</span>
+          <span>{formatNumber(patch._count.comment || 0)}</span>
         </div>
       </Tooltip>
     </div>
