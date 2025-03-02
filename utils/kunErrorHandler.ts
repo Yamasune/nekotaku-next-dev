9b5cb96e0b8d909e79455ea77d4a78702218edf7
KUN1007 <yuyuyukunkunkun@gmail.com>
2025-03-03 01:55:36 +0800

    fix: kunErrorHandler parse string error

--- utils/kunErrorHandler.ts
+++ utils/kunErrorHandler.ts
@@ -5,7 +5,25 @@ export const kunErrorHandler = <T>(
   callback: (res: T) => void
 ) => {
   if (typeof res === 'string') {
-    toast.error(res)
+    try {
+      const parsedRes = JSON.parse(res)
+
+      if (Array.isArray(parsedRes)) {
+        const errorMessages = parsedRes
+          .map((err) =>
+            typeof err.message === 'string' ? err.message : '发生未知错误'
+          )
+          .join('\n')
+        toast.error(errorMessages)
+      } else if (typeof parsedRes === 'object' && parsedRes.message) {
+        toast.error(parsedRes.message)
+      } else {
+        toast.error('发生未知错误')
+      }
+      // eslint-disable-next-line no-unused-vars
+    } catch (e) {
+      toast.error(res)
+    }
   } else {
     callback(res)
   }
