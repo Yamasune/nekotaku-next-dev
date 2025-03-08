381167e6b90beb071d8681956ed28e7aac818ec8
KUN1007 <yuyuyukunkunkun@gmail.com>
2025-03-08 15:19:13 +0800

    mod: disable tag pages indexing

--- app/tag/[id]/metadata.ts
+++ app/tag/[id]/metadata.ts
@@ -1,27 +1,8 @@
-import { kunMoyuMoe } from '~/config/moyu-moe'
+import { generateNullMetadata } from '~/utils/noIndex'
 import type { Metadata } from 'next'
 import type { TagDetail } from '~/types/api/tag'
 
 export const generateKunMetadataTemplate = (tag: TagDetail): Metadata => {
-  return {
-    title: `标签 - ${tag.name}`,
-    description: tag.introduction,
-    openGraph: {
-      title: `标签 - ${tag.name}`,
-      description: tag.introduction,
-      type: 'article',
-      publishedTime: new Date(tag.created).toISOString(),
-      modifiedTime: new Date(tag.created).toISOString(),
-      tags: tag.alias
-    },
-    twitter: {
-      card: 'summary',
-      title: `标签 - ${tag.name}`,
-      description: tag.introduction
-    },
-    alternates: {
-      canonical: `${kunMoyuMoe.domain.main}/tag/${tag.id}`
-    },
-    keywords: [tag.name, ...tag.alias]
-  }
+  const title = `标签 - ${tag.name}`
+  return generateNullMetadata(title)
 }
--- app/tag/metadata.ts
+++ app/tag/metadata.ts
@@ -1,22 +1,6 @@
-import { kunMoyuMoe } from '~/config/moyu-moe'
-import { SUPPORTED_TYPE_MAP } from '~/constants/resource'
+import { generateNullMetadata } from '~/utils/noIndex'
 import type { Metadata } from 'next'
 
 export const kunMetadata: Metadata = {
-  title: `Galgame 标签`,
-  description: `所有的 Galgame 标签, ${Object.values(SUPPORTED_TYPE_MAP).toString()} 下载`,
-  openGraph: {
-    title: `Galgame 标签`,
-    description: `所有的 Galgame 标签, ${Object.values(SUPPORTED_TYPE_MAP).toString()} 下载`,
-    type: 'website',
-    images: kunMoyuMoe.images
-  },
-  twitter: {
-    card: 'summary_large_image',
-    title: `Galgame 标签`,
-    description: `所有的 Galgame 标签, ${Object.values(SUPPORTED_TYPE_MAP).toString()} 下载`
-  },
-  alternates: {
-    canonical: `${kunMoyuMoe.domain.main}/tag`
-  }
+  ...generateNullMetadata(`Galgame 标签`)
 }
