17f6c3c14030a996a8d376ad7a33dc2beaaad49c
KUN1007 <yuyuyukunkunkun@gmail.com>
2025-04-01 03:18:54 +0800

    feat: user favorite folder schema

--- components/patch/header/button/FavoriteButton.tsx
+++ components/patch/header/button/FavoriteButton.tsx
@@ -27,7 +27,7 @@ export const FavoriteButton = ({ patchId, isFavorite }: Props) => {
     }
 
     setLoading(true)
-    const res = await kunFetchPut<KunResponse<boolean>>('/patch/like', {
+    const res = await kunFetchPut<KunResponse<boolean>>('/patch/favorite', {
       patchId
     })
 
