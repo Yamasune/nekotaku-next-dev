2001bb9e50936e0ceb0bf5be38becad6d91fe516
Palentum <79781425+Palentum@users.noreply.github.com>
2025-05-12 14:21:01 +0800

    mod: 修改discord图标大小

--- components/kun/icons/Telegram.tsx
+++ components/kun/icons/Telegram.tsx
@@ -7,7 +7,7 @@ export const Telegram = (props: SVGProps<SVGSVGElement>) => {
       xmlns="http://www.w3.org/2000/svg"
       width="24"
       height="24"
-      viewBox="0 0 800 800"
+      viewBox="0 0 24 24"
       {...props}
     >
       <path
