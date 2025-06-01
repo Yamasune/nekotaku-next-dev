612502f38a4f20408a52081dead037dc04aa5e35
flipwind <flipwind.t@gmail.com>
2025-06-01 21:40:49 +0800

    perf: switch another tips when NSFW enabled

--- components/tag/TagList.tsx
+++ components/tag/TagList.tsx
@@ -3,6 +3,7 @@ import { TagCard } from './Card'
 import { KunNull } from '~/components/kun/Null'
 import { KunLoading } from '~/components/kun/Loading'
 import type { Tag as TagType } from '~/types/api/tag'
+import { useSettingStore } from '~/store/settingStore'
 
 interface TagListProps {
   tags: TagType[]
@@ -11,13 +12,23 @@ interface TagListProps {
 }
 
 export const TagList = ({ tags, loading, searching }: TagListProps) => {
+  const settings = useSettingStore((state) => state.data)
+  const isNSFWEnabled =
+    settings.kunNsfwEnable === 'nsfw' || settings.kunNsfwEnable === 'all'
+
   if (loading) {
     return <KunLoading hint="正在获取标签数据..." />
   }
 
   if (!searching && tags.length === 0) {
     return (
-      <KunNull message="未找到相关内容, 请尝试使用游戏的日文原名搜索或打开 NSFW" />
+      <KunNull
+        message={
+          isNSFWEnabled
+            ? '您已启用显示 NSFW 内容, 但未找到相关内容, 请尝试使用游戏的日文原名搜索'
+            : '未找到相关内容, 请尝试使用游戏的日文原名搜索或打开 NSFW'
+        }
+      />
     )
   }
 
